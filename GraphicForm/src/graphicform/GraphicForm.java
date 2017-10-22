7d0f967004fd97db2b09cda4e41df94a23d9dd0c
fabio <fabio@Compu-Casa>
2017-10-22 14:57:44 -0600

    Ya imprime la tabla

--- GraphicForm/src/graphicform/GraphicForm.java
+++ GraphicForm/src/graphicform/GraphicForm.java
@@ -5,10 +5,16 @@
  */
 package graphicform;
 
+import informationmanagement.InformationManagement;
+import informationmanagement.InstrumentType;
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Dimension;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
@@ -55,15 +61,32 @@ public class GraphicForm extends JFrame{
         fixes(getContentPane());
         setResizable(true);
         setSize(400, 600);
+         setMinimumSize(new Dimension(400, 300));
+        setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         
     }
     
     private void fixes(Container c){
         
-            c.setLayout(new BorderLayout());
-            
-           
+         try {
+             c.setLayout(new BorderLayout());
+             c.add(BorderLayout.CENTER,
+                     mainPanel = new JPanel());
+             mainPanel.setLayout(new BorderLayout());
+             mainPanel.add(BorderLayout.CENTER,
+                     new JScrollPane(
+                             table
+                                     = new JTable(
+                                             InformationManagement.getInstance().obtenerTabla(),
+                                             InstrumentType.getDescription()),
+                             JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                             JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED
+                     )
+             );
+         } catch (InstantiationException | ClassNotFoundException | IllegalAccessException ex) {
+             Logger.getLogger(GraphicForm.class.getName()).log(Level.SEVERE, null, ex);
+         }
         
     }
     
