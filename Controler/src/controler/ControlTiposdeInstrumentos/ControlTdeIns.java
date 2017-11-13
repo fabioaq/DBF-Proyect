1afdd7dd6dee954ff1f67b6dedd42f8ef3ed0009
fabio <fabio@Compu-Casa>
2017-11-13 03:40:04 -0600

    Separacion de clases y arreglos multiples

--- Controler/src/controler/ControlTiposdeInstrumentos/ControlTdeIns.java
+++ Controler/src/controler/ControlTiposdeInstrumentos/ControlTdeIns.java
@@ -5,10 +5,26 @@
  */
 package controler.ControlTiposdeInstrumentos;
 
+import informationmanagement.TiposDeInstrumentos.InsTypeGestor;
+import informationmanagement.TiposDeInstrumentos.InstrumentType;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author fabio
  */
 public class ControlTdeIns {
+    
+    public boolean agregarIns(String c, String n, String u){
+        InstrumentType i = new InstrumentType(c, n, u);
+        try {
+            InsTypeGestor.getInstancia().agregar(i);
+            return true;
+        } catch (InstantiationException | ClassNotFoundException | IllegalAccessException ex) {
+            Logger.getLogger(ControlTdeIns.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
     
 }
